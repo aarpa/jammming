f4975ab09b2c7626cc61cf7c020a91af2849aa27
Arpa Imran <aarpa128@gmail.com>
2019-08-05 22:52:35 -0700

    Updates

---
+++ precache-manifest.a96c3ae3ce616550f433ef368a1355c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  },
+  {
+    "revision": "53ccb5121c849fce0dc6",
+    "url": "/jammming/static/js/runtime~main.53ccb512.js"
+  },
+  {
+    "revision": "10745b1e19a928117c30",
+    "url": "/jammming/static/js/main.10745b1e.chunk.js"
+  },
+  {
+    "revision": "ea224882c32aae1f4fdf",
+    "url": "/jammming/static/js/1.ea224882.chunk.js"
+  },
+  {
+    "revision": "10745b1e19a928117c30",
+    "url": "/jammming/static/css/main.d47fded2.chunk.css"
+  },
+  {
+    "revision": "3e4c237e95043c9c92f0d7af82cb3aaa",
+    "url": "/jammming/index.html"
+  }
+];
